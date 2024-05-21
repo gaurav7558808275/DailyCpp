ef0bed7511d3bcc14f3d7ed7bba4859094c11b23
gaurav phadke <grvgaurav03@gmail.com>
2024-05-21 22:21:44 +0530

    LinkedList-1

---
+++ CC++codes/LinkedList-1.c
@@ -0,0 +1,52 @@
+#include"stdio.h"
+#include "stdlib.h"
+
+
+struct node{
+    
+    int data;
+    struct node * next;
+
+};
+int printLinkedList(struct node * p)
+{
+    while(p != NULL)
+    {
+        printf("%d\n",p->data);
+        p = p->next;
+    }
+    return 0;
+}
+int main()
+{
+
+    struct node * head; 
+    struct node * One = NULL;
+    struct node * Two = NULL;
+    struct node * Three = NULL;
+
+    One = malloc(sizeof(struct node));
+    Two = malloc(sizeof(struct node));
+    Three = malloc(sizeof(struct node));
+
+    One->data = 1;
+    Two->data = 2;
+    Three->data = 3;
+
+    head = One;
+    One->next = Two;
+    Two->next = Three;
+    Three->next = NULL;
+
+    
+    printLinkedList(head);
+
+    free(One);
+    free(Two);
+    free(Three);
+
+    printf("it s working");
+
+}
+
+
